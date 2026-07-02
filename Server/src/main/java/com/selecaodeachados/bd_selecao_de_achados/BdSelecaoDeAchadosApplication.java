8e2848287726dafdf6e1cb8bc942213fc1878701
Reginaldo345 <juniorfb98@gmail.com>
2026-07-02 08:18:13 -0300

    Primeira versão

--- Server/src/main/java/com/selecaodeachados/bd_selecao_de_achados/BdSelecaoDeAchadosApplication.java
+++ Server/src/main/java/com/selecaodeachados/bd_selecao_de_achados/BdSelecaoDeAchadosApplication.java
@@ -3,6 +3,7 @@ package com.selecaodeachados.bd_selecao_de_achados;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.URI;
 
 @SpringBootApplication
 public class BdSelecaoDeAchadosApplication {
@@ -17,6 +18,31 @@ public class BdSelecaoDeAchadosApplication {
 				System.setProperty(entry.getKey(), entry.getValue());
 			}
 		});
+
+		String databaseUrl = System.getenv("DATABASE_URL");
+		if (databaseUrl != null && !databaseUrl.isBlank()) {
+			try {
+				URI uri = new URI(databaseUrl);
+				String userInfo = uri.getUserInfo();
+				if (userInfo != null && userInfo.contains(":")) {
+					String[] parts = userInfo.split(":", 2);
+					String jdbcUrl = "jdbc:postgresql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath()
+							+ "?sslmode=require";
+
+					if (System.getProperty("spring.datasource.url") == null) {
+						System.setProperty("spring.datasource.url", jdbcUrl);
+					}
+					if (System.getProperty("spring.datasource.username") == null) {
+						System.setProperty("spring.datasource.username", parts[0]);
+					}
+					if (System.getProperty("spring.datasource.password") == null) {
+						System.setProperty("spring.datasource.password", parts[1]);
+					}
+				}
+			} catch (Exception e) {
+				System.err.println("Falha ao parsear DATABASE_URL: " + e.getMessage());
+			}
+		}
 
 		SpringApplication.run(BdSelecaoDeAchadosApplication.class, args);
 	}
